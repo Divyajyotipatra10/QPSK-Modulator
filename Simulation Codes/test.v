a6ddd29b336c7653d94616870bd35e3c40c969e6
Divyajyoti Patra <djpatra07@gmail.com>
2024-05-21 16:07:08 +0530

    Testbench to check the output
    It is a modifiable test bench.

---
+++ Simulation Codes/test.v
@@ -0,0 +1,20 @@
+module test(
+
+    );
+    reg [7:0]data;
+    reg clk,ld;
+    wire [7:0]qpskout;
+    QPSK dut(data,clk,ld,qpskout);
+    initial begin
+        clk<=1'b0;
+        forever #10 clk<=~clk;
+    end
+    initial begin
+        ld<=1'b1;
+        data<=8'b00101100;//0110//0010
+        #25
+        ld<=1'b0;
+        #9000
+        $stop;
+    end
+endmodule
